5771f3e9bb337d4ad5087afd7a3dfca824af3582
ningsheng <ningsheng@sina.cn>
2015-10-19 22:27:17 +0800

    kjhg

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var mongoStore = require('connect-mongo')(session);
+var logger = require('morgan');
+var port = 3000
+var app = express()
+var fs = require('fs')
+var dbUrl = 'mongodb://localhost/movie'
+
+mongoose.connect(dbUrl)
+
+// models loading
+var models_path = __dirname + '/app/models'
+var walk = function(path) {
+    fs
+        .readdirSync(path)
+        .forEach(function(file) {
+            var newPath = path + '/' + file
+            var stat = fs.statSync(newPath)
+
+            if (stat.isFile()) {
+                if (/(.*)\.(js|coffee)/.test(file)) {
+                    require(newPath)
+                }
+            }
+            else if (stat.isDirectory()) {
+                walk(newPath)
+            }
+        })
+}
+walk(models_path)
+app.set('views', './app/views/pages')
+app.set('view engine', 'jade')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'ning',
+    store: new mongoStore({
+        url: dbUrl,
+        collection: 'sessions'
+    })
+}));
+
+if ('development' === app.get('env')) {
+    app.set('showStackError', true)
+    app.use(logger(':method :url :status'))
+    app.locals.pretty = true
+    //mongoose.set('debug', true)
+}
+
+require('./config/routes')(app)
+
+app.listen(port)
+app.locals.moment = require('moment')
+
+console.log('imooc started on port ' + port)
+
